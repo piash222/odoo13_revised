cd68a89eb7f8af16045c812e189f6cf49b0d1812
Md. Shihab Uddin <piash222@gmail.com>
2021-02-21 16:35:25 +0600

    rec name for Models

--- custom_addons/om_hospital/patient.py
+++ custom_addons/om_hospital/patient.py
@@ -4,7 +4,8 @@ from odoo import fields, models, api
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = "Patient Record"
-
+    _rec_name = "patient_name"
+ 
     patient_name = fields.Char(string='Name', required=True)
     patient_age = fields.Char(string='Age')
     notes = fields.Text(string="Notes")
