66e53c2055472eb635ad87dc2f7d116f71ff52fa
Md. Shihab Uddin <piash222@gmail.com>
2021-02-22 22:26:45 +0600

    13. Write Compute Field and its Function

--- custom_addons/om_hospital/patient.py
+++ custom_addons/om_hospital/patient.py
@@ -8,7 +8,7 @@ class HospitalPatient(models.Model):
     _rec_name = "patient_name"
 
     patient_name = fields.Char(string='Name', required=True)
-    patient_age = fields.Char(string='Age')
+    patient_age = fields.Integer(string='Age')
     notes = fields.Text(string="Notes")
     image = fields.Binary(string="Image")
     name_seq = fields.Char(string="Patient ID", required=True, copy=False,
@@ -17,7 +17,10 @@ class HospitalPatient(models.Model):
         string='Gender',
         selection=[('male', 'Male'),
                    ('fe_male', 'Female'), ], default='male')
-
+    age_group = fields.Selection(
+        string='Age Group',
+        selection=[('major', 'Major'),
+                   ('minor', 'Minor')], compute='set_age_group')
 
     @api.model
     def create(self, vals_list):
@@ -25,3 +28,11 @@ class HospitalPatient(models.Model):
             vals_list['name_seq'] = self.env['ir.sequence'].next_by_code('hospital.patient.sequence') or _('New')
         result = super(HospitalPatient, self).create(vals_list)
         return result
+
+    @api.depends('patient_age')
+    def set_age_group(self):
+        for rec in self:
+            if rec.patient_age < 18:
+                rec.age_group = 'minor'
+            else:
+                rec.age_group = "major"
