3feedd2232a587ac0480bb29cfd64a6d4bc2fb5e
Md. Shihab Uddin <piash222@gmail.com>
2021-03-09 08:38:35 +0600

    42. Create Record From Code

--- custom_addons/om_hospital/wizards/create_appointment.py
+++ custom_addons/om_hospital/wizards/create_appointment.py
@@ -6,3 +6,12 @@ class CreateAppointment(models.TransientModel):
 
     patient_id = fields.Many2one('hospital.patient', string="patient")
     appointment_date = fields.Date(string="appointment Date")
+
+    def create_appointment(self):
+        vals = {
+            'patient_id': self.patient_id.id,
+            'patient_age': self.patient_id.patient_age,
+            'notes': "created from menu",
+            'appointment_date': self.appointment_date
+        }
+        self.env['hospital.appointment'].create(vals)
