68a0e014bca3db3f08958851a478aca90e906dd6
Md. Shihab Uddin <piash222@gmail.com>
2021-03-30 19:39:40 +0600

    77. Web Controllers Auth Public and Auth User Differenc

--- custom_addons/om_hospital/controllers/main.py
+++ custom_addons/om_hospital/controllers/main.py
@@ -4,7 +4,7 @@ from odoo.http import request
 
 class Hospital(http.Controller):
     # Sample Controller Created
-    @http.route('/hospital/patient/', website=True, auth='public')
+    @http.route('/hospital/patient/', website=True, auth='auth')
     def hospital_patient(self, **kwargs):
         # return 'hello world'
         patients = request.env['hospital.patient'].sudo().search([])
